f1b07f0379746694aa1050e34b4a0c6830195f1d
João Victor Silva <joaotecnico771@gmail.com>
2025-01-15 13:04:38 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,1 +1,17 @@
-nome=input("Digite seu nome:")
+window.addEventListener("scroll", function () {
+    var header = this.document.querySelector('#header');
+    var text = this.document.querySelector('#name');
+
+    if (this.window.scrollY > 0) {
+        text.style.color = 'white';
+        header.classList.add('rolagem');
+    } else {
+        text.style.color = 'black';
+        header.classList.remove('rolagem');
+    }
+});
+
+function enviar() {
+    var infoUm = document.getElementById('infoUm').value;
+    alert(`Olá ${infoUm}, obrigado pela visita. Caso queira entrar em contato comigo, me mande um e-mail!`);
+}
